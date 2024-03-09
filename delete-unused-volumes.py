0f495f4ae71fb9146505dc426eaa2095551234e9
vamsi.chunduru <cvamsikrishna11@gmail.com>
2024-03-09 21:34:35 +0530

    erfa

--- delete-unused-volumes.py
+++ delete-unused-volumes.py
@@ -6,30 +6,33 @@ client = boto3.client("ec2", region_name="us-east-1")
 
 # Use the describe_volumes method to list all volumes
 # Filters are used to list only volumes that are in the 'available' state
-response = client.describe_volumes(
-    Filters=[
-        {
-            'Name': 'status', # The filter name, in this case, filtering by volume status
-            'Values': [
-                'available', # The filter value, specifying we only want volumes that are 'available'
-            ]
-        }
-    ],
-)
-
-print("Available Volumes Response: ", response)
-
-# Iterate through the list of available volumes returned by the describe_volumes call
-for i in response["Volumes"]:
-    # Print the Volume ID of each available volume
-    print("Deleting Volume with ID ::::::::::::::::::::::::::", i["VolumeId"])
-    
-    # Use the delete_volume method to delete the volume specified by its VolumeId
-    # This operation is irreversible and will delete the volume permanently
-    response = client.delete_volume(
-        VolumeId=i["VolumeId"]
+def lambda_handler(event, context):
+    response = client.describe_volumes(
+        Filters=[
+            {
+                'Name': 'status', # The filter name, in this case, filtering by volume status
+                'Values': [
+                    'available', # The filter value, specifying we only want volumes that are 'available'
+                ]
+            }
+        ],
     )
-    
-    # Print the response from the delete_volume call
-    # This usually contains information about the deletion request
-    print(response)
+
+    print("Available Volumes Response: ", response)
+
+    # Iterate through the list of available volumes returned by the describe_volumes call
+    for i in response["Volumes"]:
+        # Print the Volume ID of each available volume
+        print("Deleting Volume with ID ::::::::::::::::::::::::::", i["VolumeId"])
+        
+        # Use the delete_volume method to delete the volume specified by its VolumeId
+        # This operation is irreversible and will delete the volume permanently
+        response = client.delete_volume(
+            VolumeId=i["VolumeId"]
+        )
+        
+        # Print the response from the delete_volume call
+        # This usually contains information about the deletion request
+        print(response)
+
+
